4fbcf93296f0701122102cf4efd4b5e6601a3d07
deztidev <martinianosanchi@gmail.com>
2021-12-28 11:42:26 -0300

    Fix firefox bugs

--- src/components/App.js
+++ src/components/App.js
@@ -11,19 +11,19 @@ import NotFound from "../pages/NotFound";
 import "../assets/styles/index.scss";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/previous-launches" component={PreviousLaunches} />
-          <Route exact path="/next-launches" component={NextLaunches} />
-          <Route exact path="/rockets" component={Rockets} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/previous-launches" component={PreviousLaunches} />
+					<Route exact path="/next-launches" component={NextLaunches} />
+					<Route exact path="/rockets" component={Rockets} />
+					<Route component={NotFound} />
+				</Switch>
+			</Layout>
+		</BrowserRouter>
+	);
 };
 
 export default App;
